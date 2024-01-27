81b8682e2b949209f4431f0dfe96c036c6ada34c
SzabHUN <45877267+SzabHUN@users.noreply.github.com>
2024-01-27 03:29:55 +0100

    főmenü_layout csatlakozás

--- app/src/main/java/com/szabhun/sgttteam/hangman/AboutActivity.java
+++ app/src/main/java/com/szabhun/sgttteam/hangman/AboutActivity.java
@@ -13,6 +13,12 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        credits = findViewById(R.id.TV_AB_CRDS);
+        description = findViewById(R.id.TV_AB_Desc);
+        sztiltle = findViewById(R.id.TV_Sz_Tiltle);
+        sptiltle = findViewById(R.id.TV_Sp_Tiltle);
+        ltiltle = findViewById(R.id.TV_L_Tiltle);
 
     }
 }
